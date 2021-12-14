84ec602e8288f263ab446fb4357bf491537e50da
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2021-12-14 14:33:55 +0200

    Update lab1 task2 (single-threaded solution)

---
+++ lab1/src/com/nikita/Task2.java
@@ -0,0 +1,50 @@
+package com.nikita;
+
+import com.nikita.list.SkipList;
+
+public class Task2 implements Runnable {
+    private SkipList<Integer> list;
+
+    private static int getRandomInt(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    public Task2(SkipList<Integer> list) {
+        this.list = list;
+    }
+
+    public static void main(String[] args) {
+        SkipList<Integer> list = new SkipList<>();
+
+        Thread thread1 = new Thread(new Task2(list));
+        Thread thread2 = new Thread(new Task2(list));
+        Thread thread3 = new Thread(new Task2(list));
+
+        thread1.start();
+        // thread2.start();
+        // thread3.start();
+
+        try {
+            thread1.join();
+            // thread2.join();
+            // thread3.join();
+        } catch (InterruptedException err) {
+            System.err.println(err);
+        }
+
+        System.out.println(">>> List contains 0: " + list.contains(0));
+        System.out.println(">>> List contains 1: " + list.contains(1));
+        System.out.println(">>> List contains 2: " + list.contains(2));
+        System.out.println(">>> List contains 3: " + list.contains(3));
+        System.out.println(">>> List contains 4: " + list.contains(4));
+
+        System.out.println(list.traverse());
+    }
+
+    public void run() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        // list.remove(getRandomInt(1, 3));
+    }
+}
--- lab1/src/com/nikita/list/SkipList.java
+++ lab1/src/com/nikita/list/SkipList.java
@@ -2,10 +2,11 @@ package com.nikita.list;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
-public class SkipList<T> {
+public class SkipList<T extends Comparable<T>> {
     private int maxHeight;
     private Node<T> headTop;
 
@@ -46,14 +47,29 @@ public class SkipList<T> {
         }
     }
 
-    private Node<T> getHeadBottom() {
+    private Node<T> getHeadBottomNode() {
+        return getHeadNodeByIndex(0);
+    }
+
+    private Node<T> getHeadNodeByIndex(int index) {
         Node<T> currNode = headTop;
 
-        while (currNode.getLowerNode() != null) {
+        for (int i = maxHeight - 1; i > index; i--) {
             currNode = currNode.getLowerNode();
         }
 
         return currNode;
+    }
+
+    private int getNodeHeight() {
+        Random random = new Random();
+        int level = 1;
+
+        while (level < maxHeight && Math.abs(random.nextInt()) % 2 == 0) {
+            level++;
+        }
+
+        return level;
     }
 
     public SkipList() {
@@ -71,15 +87,73 @@ public class SkipList<T> {
     }
 
     public void add(T elem) {
+        int height = getNodeHeight();
 
+        Node<T> currNode = getHeadNodeByIndex(height - 1);
+        Node<T> prevAddedNode = null;
+
+        while (currNode != null) {
+            Node<T> rightNode = currNode.getRightNode();
+
+            /**
+             * Right node is less than a node to add.
+             * Proceed to the right node.
+             */
+            if (
+                rightNode != null &&
+                rightNode.getValue().compareTo(elem) != 1
+            ) {
+                currNode = rightNode;
+            } else {
+                Node<T> node = new Node(elem);
+
+                node.setRightNode(rightNode);
+                currNode.setRightNode(node);
+
+                currNode = currNode.getLowerNode();
+
+                /**
+                 * Set added node as a lower node for
+                 * previously added node.
+                 */
+                if (prevAddedNode != null) {
+                    prevAddedNode.setLowerNode(node);
+                }
+
+                prevAddedNode = node;
+            }
+        }
     }
 
     public void remove(int index) {
 
     }
 
+    public boolean contains(T elem) {
+        Node<T> currNode = getHeadBottomNode().getRightNode();
+
+        while (currNode != null && !currNode.getValue().equals(elem)) {
+            Node<T> rightNode = currNode.getRightNode();
+
+            /**
+             * Right node is less than a node to add.
+             * Proceed to the right node.
+             */
+            if (
+                rightNode != null &&
+                rightNode.getValue().compareTo(elem) != 1
+            ) {
+                currNode = rightNode;
+            } else {
+                currNode = currNode.getLowerNode();
+            }
+        }
+
+        return currNode != null;
+    }
+
     public String traverse() {
-        Node<T> currNode = getHeadBottom().getRightNode();
+        Node<T> currNode = getHeadBottomNode().getRightNode();
         List<String> out = new LinkedList<>();
 
         while (currNode != null) {
